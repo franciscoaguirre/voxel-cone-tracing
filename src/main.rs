65a2175a89346762d18b15fc10999ad7030e8af4
Francisco Aguirre <franciscoaguirreperez@gmail.com>
2022-05-10 08:50:59 -0300

    Setting up OpenGL

---
+++ src/main.rs
@@ -0,0 +1,125 @@
+extern crate glfw;
+use self::glfw::{Action, Context, Key};
+
+extern crate gl;
+use self::gl::types::*;
+
+mod shader;
+use shader::Shader;
+
+use std::mem;
+use std::os::raw::c_void;
+use std::ptr;
+use std::sync::mpsc::Receiver;
+
+// Settings
+const SOURCE_WIDTH: u32 = 800;
+const SOURCE_HEIGHT: u32 = 600;
+
+fn main() {
+    // GLFW: Setup
+    let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
+    glfw.window_hint(glfw::WindowHint::ContextVersion(3, 3));
+    glfw.window_hint(glfw::WindowHint::OpenGlProfile(
+        glfw::OpenGlProfileHint::Core,
+    ));
+
+    // GLFW: Window creation
+    let (mut window, events) = glfw
+        .create_window(
+            SOURCE_WIDTH,
+            SOURCE_HEIGHT,
+            "LearnOpenGL",
+            glfw::WindowMode::Windowed,
+        )
+        .expect("Failed to create GLFW window");
+
+    window.make_current();
+    window.set_key_polling(true);
+    window.set_framebuffer_size_polling(true);
+
+    // GL: Load all OpenGL function pointers
+    gl::load_with(|symbol| window.get_proc_address(symbol) as *const _);
+
+    // Set shader program
+    let (our_shader, vao) = unsafe {
+        let our_shader = Shader::new("src/shaders/simple.vs", "src/shaders/simple.fs");
+
+        // HINT: type annotation is crucial since default for float literals is f64
+        let vertices: [f32; 18] = [
+            // positions         // colors
+            0.5, -0.5, 0.0, 1.0, 0.0, 0.0, // bottom right
+            -0.5, -0.5, 0.0, 0.0, 1.0, 0.0, // bottom left
+            0.0, 0.5, 0.0, 0.0, 0.0, 1.0, // top
+        ];
+        let (mut vbo, mut vao) = (0, 0);
+        gl::GenVertexArrays(1, &mut vao);
+        gl::GenBuffers(1, &mut vbo);
+        // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+        gl::BindVertexArray(vao);
+
+        gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
+        gl::BufferData(
+            gl::ARRAY_BUFFER,
+            (vertices.len() * mem::size_of::<GLfloat>()) as GLsizeiptr,
+            &vertices[0] as *const f32 as *const c_void,
+            gl::STATIC_DRAW,
+        );
+
+        let stride = 6 * mem::size_of::<GLfloat>() as GLsizei;
+        // position attribute
+        gl::VertexAttribPointer(0, 3, gl::FLOAT, gl::FALSE, stride, ptr::null());
+        gl::EnableVertexAttribArray(0);
+        // color attribute
+        gl::VertexAttribPointer(
+            1,
+            3,
+            gl::FLOAT,
+            gl::FALSE,
+            stride,
+            (3 * mem::size_of::<GLfloat>()) as *const c_void,
+        );
+        gl::EnableVertexAttribArray(1);
+
+        // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+        // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+        // gl::BindVertexArray(0);
+
+        (our_shader, vao)
+    };
+
+    // Render loop
+    while !window.should_close() {
+        // Events
+        process_events(&mut window, &events);
+
+        // Render
+        unsafe {
+            gl::ClearColor(0.2, 0.3, 0.3, 1.0);
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+
+            // Render triangle
+            our_shader.useProgram();
+            gl::BindVertexArray(vao);
+            gl::DrawArrays(gl::TRIANGLES, 0, 3);
+        }
+
+        // GLFW: Swap buffers and poll I/O events
+        window.swap_buffers();
+        glfw.poll_events();
+    }
+}
+
+fn process_events(window: &mut glfw::Window, events: &Receiver<(f64, glfw::WindowEvent)>) {
+    for (_, event) in glfw::flush_messages(events) {
+        match event {
+            glfw::WindowEvent::FramebufferSize(width, height) => unsafe {
+                gl::Viewport(0, 0, width, height)
+            },
+            glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
+                window.set_should_close(true)
+            }
+            _ => {}
+        }
+    }
+}
