1abcd676ad9ab3cd34ac155aaeba5ea34eff0e94
Francisco Aguirre <franciscoaguirreperez@gmail.com>
2022-05-25 18:53:26 -0300

    Finished voxel fragment list, not working

---
+++ src/constants.rs
@@ -0,0 +1,4 @@
+pub const SOURCE_WIDTH: i32 = 1200;
+pub const SOURCE_HEIGHT: i32 = 800;
+
+pub const VOXEL_DIMENSION: i32 = 256;
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,24 @@
-extern crate glfw;
-use glfw::{Action, Context, Key};
-
-extern crate gl;
-
 extern crate c_str_macro;
-use c_str_macro::c_str;
+extern crate gl;
+extern crate glfw;
+use glfw::Context;
 
-use cgmath::{perspective, vec3, Deg, Matrix4, Point3};
+use cgmath::Point3;
 
 mod shader;
 use shader::Shader;
 
 mod camera;
-use camera::{Camera, Camera_Movement};
+use camera::Camera;
 
 mod mesh;
 
 mod model;
 use model::Model;
 
+mod common;
+mod constants;
 mod voxelization;
-
-use std::sync::mpsc::Receiver;
-
-// Settings
-const SOURCE_WIDTH: u32 = 1200;
-const SOURCE_HEIGHT: u32 = 800;
 
 fn main() {
     // Camera setup
@@ -35,8 +28,8 @@ fn main() {
     };
 
     let mut first_mouse = true;
-    let mut last_x: f32 = SOURCE_WIDTH as f32 / 2.0;
-    let mut last_y: f32 = SOURCE_HEIGHT as f32 / 2.0;
+    let mut last_x: f32 = constants::SOURCE_WIDTH as f32 / 2.0;
+    let mut last_y: f32 = constants::SOURCE_HEIGHT as f32 / 2.0;
 
     // Timing
     let mut delta_time: f32;
@@ -52,8 +45,8 @@ fn main() {
     // GLFW: Window creation
     let (mut window, events) = glfw
         .create_window(
-            SOURCE_WIDTH,
-            SOURCE_HEIGHT,
+            constants::SOURCE_WIDTH as u32,
+            constants::SOURCE_HEIGHT as u32,
             "LearnOpenGL",
             glfw::WindowMode::Windowed,
         )
@@ -74,8 +67,8 @@ fn main() {
         gl::Enable(gl::DEPTH_TEST);
 
         let our_shader = Shader::new(
-            "src/shaders/model_loading.vs",
-            "src/shaders/model_loading.fs",
+            "src/shaders/model_loading.vert.glsl",
+            "src/shaders/model_loading.frag.glsl",
         );
 
         let our_model = Model::new("assets/cow.obj");
@@ -90,7 +83,7 @@ fn main() {
         last_frame = current_frame;
 
         // Events
-        process_events(
+        common::process_events(
             &events,
             &mut first_mouse,
             &mut last_x,
@@ -99,90 +92,35 @@ fn main() {
         );
 
         // Input
-        process_input(&mut window, delta_time, &mut camera);
+        common::process_input(&mut window, delta_time, &mut camera);
 
         // Render
         unsafe {
             gl::ClearColor(0.1, 0.1, 0.1, 1.0);
             gl::Clear(gl::COLOR_BUFFER_BIT | gl::DEPTH_BUFFER_BIT);
 
-            our_shader.useProgram();
+            // our_shader.useProgram();
+            //
+            // let projection: Matrix4<f32> = perspective(
+            //     Deg(camera.Zoom),
+            //     SOURCE_WIDTH as f32 / SOURCE_HEIGHT as f32,
+            //     0.1,
+            //     10000.0,
+            // );
+            // let view = camera.GetViewMatrix();
+            // our_shader.setMat4(c_str!("projection"), &projection);
+            // our_shader.setMat4(c_str!("view"), &view);
+            //
+            // let mut model = Matrix4::<f32>::from_translation(vec3(0.0, -1.75, 0.0));
+            // model = model * Matrix4::from_scale(0.2); // i
+            // our_shader.setMat4(c_str!("model"), &model);
+            // our_model.Draw(&our_shader);
 
-            let projection: Matrix4<f32> = perspective(
-                Deg(camera.Zoom),
-                SOURCE_WIDTH as f32 / SOURCE_HEIGHT as f32,
-                0.1,
-                10000.0,
-            );
-            let view = camera.GetViewMatrix();
-            our_shader.setMat4(c_str!("projection"), &projection);
-            our_shader.setMat4(c_str!("view"), &view);
-
-            let mut model = Matrix4::<f32>::from_translation(vec3(0.0, -1.75, 0.0));
-            model = model * Matrix4::from_scale(0.2); // i
-            our_shader.setMat4(c_str!("model"), &model);
-            our_model.Draw(&our_shader);
+            voxelization::build_voxel_fragment_list();
         }
 
         // GLFW: Swap buffers and poll I/O events
         window.swap_buffers();
         glfw.poll_events();
-    }
-}
-
-pub fn process_events(
-    events: &Receiver<(f64, glfw::WindowEvent)>,
-    first_mouse: &mut bool,
-    last_x: &mut f32,
-    last_y: &mut f32,
-    camera: &mut Camera,
-) {
-    for (_, event) in glfw::flush_messages(events) {
-        match event {
-            glfw::WindowEvent::FramebufferSize(width, height) => {
-                // make sure the viewport matches the new window dimensions; note that width and
-                // height will be significantly larger than specified on retina displays.
-                unsafe { gl::Viewport(0, 0, width, height) }
-            }
-            glfw::WindowEvent::CursorPos(x_position, y_position) => {
-                let (x_position, y_position) = (x_position as f32, y_position as f32);
-                if *first_mouse {
-                    *last_x = x_position;
-                    *last_y = y_position;
-                    *first_mouse = false;
-                }
-
-                let x_offset = x_position - *last_x;
-                let y_offset = *last_y - y_position; // reversed since y-coordinates go from bottom to top
-
-                *last_x = x_position;
-                *last_y = y_position;
-
-                camera.ProcessMouseMovement(x_offset, y_offset, true);
-            }
-            glfw::WindowEvent::Scroll(_x_offset, y_offset) => {
-                camera.ProcessMouseScroll(y_offset as f32);
-            }
-            _ => {}
-        }
-    }
-}
-
-pub fn process_input(window: &mut glfw::Window, delta_time: f32, camera: &mut Camera) {
-    if window.get_key(Key::Escape) == Action::Press {
-        window.set_should_close(true)
-    }
-
-    if window.get_key(Key::W) == Action::Press {
-        camera.ProcessKeyboard(Camera_Movement::Forward, delta_time);
-    }
-    if window.get_key(Key::S) == Action::Press {
-        camera.ProcessKeyboard(Camera_Movement::Backward, delta_time);
-    }
-    if window.get_key(Key::A) == Action::Press {
-        camera.ProcessKeyboard(Camera_Movement::Left, delta_time);
-    }
-    if window.get_key(Key::D) == Action::Press {
-        camera.ProcessKeyboard(Camera_Movement::Right, delta_time);
     }
 }
