9979f529c1cd0f59c10607f20d34e84b62d2fb88
Felipe Pizzorno <felipepizzorno98@gmail.com>
2022-09-05 01:35:00 -0300

    Pizzo misc

--- src/constants.rs
+++ src/constants.rs
@@ -3,7 +3,7 @@ pub const SOURCE_HEIGHT: i32 = 1024;
 
 pub const VOXEL_DIMENSION: i32 = 256;
 
-pub const OCTREE_LEVELS: u32 = 8;
+pub const OCTREE_LEVELS: u32 = 7;
 pub const NODES_PER_TILE: u32 = 8;
 
 // For compute shaders
