d8caed02fdffe2ea99ecca398630d7d0c5c7b6da
Francisco Aguirre <franciscoaguirreperez@gmail.com>
2023-04-22 18:12:45 -0300

    Fix light transfer issue at higher levels

--- assets/shaders/octree/lightTransfer.comp.glsl
+++ assets/shaders/octree/lightTransfer.comp.glsl
@@ -3,12 +3,13 @@
 #include "./_constants.glsl"
 #include "./_helpers.glsl"
 
-layout (local_size_x = 32, local_size_y = 32, local_size_z = 1) in;
+// layout (local_size_x = 32, local_size_y = 32, local_size_z = 1) in;
+layout (local_size_x = WORKING_GROUP_SIZE, local_size_y = 1, local_size_z = 1) in;
 
 uniform layout(binding = 0, r32ui) uimageBuffer nodePoolNeighbors;
 uniform layout(binding = 1, r32ui) uimage3D photonValues;
 uniform layout(binding = 2, r32ui) uimageBuffer nodePool;
-uniform layout(binding = 3, r32f) imageBuffer debug;
+uniform layout(binding = 3, r32ui) uimageBuffer levelStartIndices;
 
 uniform usampler2D lightViewMap;
 
@@ -16,36 +17,35 @@ uniform uint axis;
 uniform uint octreeLevel;
 uniform uint voxelDimension;
 
-#include "./_traversalHelpers.glsl"
-#include "./_octreeTraversal.glsl"
+#include "./_threadNodeUtil.glsl"
 
 // Doing it this way can have concurrency problems if we run all three axis concurrently (not sure if posible, but paper does it in two passes)
 
 void main() {
-    uvec3 queryCoordinates = texelFetch(
-        lightViewMap,
-        ivec2(gl_GlobalInvocationID.xy),
-        0
-    ).xyz;
-    if (queryCoordinates == uvec3(0)) {
-        return;
-    }
-    vec3 normalizedQueryCoordinates = vec3(queryCoordinates.xyz / (float(voxelDimension) * 1.5));
+    // TODO: Optimize with the 2D node map
+    int nodeID = getThreadNode();
+    // uvec3 queryCoordinates = texelFetch(
+    //     lightViewMap,
+    //     ivec2(gl_GlobalInvocationID.xy),
+    //     0
+    // ).xyz;
+    // if (queryCoordinates == uvec3(0)) {
+    //     return;
+    // }
+    // vec3 normalizedQueryCoordinates = vec3(queryCoordinates.xyz / (float(voxelDimension) * 1.5));
     // vec3 normalizedQueryCoordinates = vec3(uvec3(128, 224, 128) / float(voxelDimension));
     // uint myOctreeLevel = 5;
-    float halfNodeSize;
-    vec3 nodeCoordinates;
-    int nodeID = traverseOctree(
-        normalizedQueryCoordinates,
-        octreeLevel,
-        nodeCoordinates,
-        halfNodeSize
-    );
-    if (nodeID == NODE_NOT_FOUND) {
-        return;
-    }
-
-    imageStore(debug, 0, vec4(float(nodeID), 0, 0, 0));
+    // float halfNodeSize;
+    // vec3 nodeCoordinates;
+    // int nodeID = traverseOctree(
+    //     normalizedQueryCoordinates,
+    //     octreeLevel,
+    //     nodeCoordinates,
+    //     halfNodeSize
+    // );
+    // if (nodeID == NODE_NOT_FOUND) {
+    //     return;
+    // }
 
     int neighborID = int(imageLoad(nodePoolNeighbors, nodeID).r);
     if (neighborID == 0) {
@@ -54,8 +54,6 @@ void main() {
 
     ivec3 brickAddress = calculateBrickCoordinates(nodeID);
     ivec3 neighborBrickAddress = calculateBrickCoordinates(neighborID);
-
-    imageStore(debug, 1, vec4(float(neighborID), 0, 0, 0));
 
     if (axis == 0) {
         for (int y = 0; y <= 2; y++) {
--- src/octree/lighting/stages/light_transfer.rs
+++ src/octree/lighting/stages/light_transfer.rs
@@ -2,7 +2,10 @@ use c_str_macro::c_str;
 use cgmath::vec3;
 use gl::types::GLuint;
 
-use crate::{config::CONFIG, helpers, octree::OctreeTextures, rendering::shader::Shader};
+use crate::{
+    config::CONFIG, constants::WORKING_GROUP_SIZE, helpers, octree::OctreeTextures,
+    rendering::shader::Shader,
+};
 
 pub struct BorderTransferPass {
     shader: Shader,
@@ -17,7 +20,12 @@ impl BorderTransferPass {
         }
     }
 
-    pub unsafe fn run(&self, textures: &OctreeTextures, octree_level: u32) {
+    pub unsafe fn run(
+        &self,
+        textures: &OctreeTextures,
+        octree_level: u32,
+        nodes_per_level: &[u32],
+    ) {
         self.shader.use_program();
 
         self.shader.set_uint(c_str!("octreeLevel"), octree_level);
@@ -30,37 +38,38 @@ impl BorderTransferPass {
 
         helpers::bind_3d_image_texture(1, textures.brick_pool_photons, gl::READ_WRITE, gl::R32UI);
         helpers::bind_image_texture(2, textures.node_pool.0, gl::READ_ONLY, gl::R32UI);
-        let (debug, buffer) = helpers::generate_texture_buffer(10, gl::R32F, 69_f32);
-        helpers::bind_image_texture(3, debug, gl::WRITE_ONLY, gl::R32F);
+        helpers::bind_image_texture(3, textures.level_start_indices.0, gl::READ_ONLY, gl::R32UI);
 
         helpers::bind_image_texture(0, textures.neighbors[0].0, gl::READ_ONLY, gl::R32UI);
         self.shader.set_uint(c_str!("axis"), 0);
-        self.shader.dispatch_xyz(vec3(
-            (CONFIG.viewport_width as f32 / 32 as f32).ceil() as u32,
-            (CONFIG.viewport_height as f32 / 32 as f32).ceil() as u32,
-            1,
-        ));
+        // self.shader.dispatch_xyz(vec3(
+        //     (CONFIG.viewport_width as f32 / 32 as f32).ceil() as u32,
+        //     (CONFIG.viewport_height as f32 / 32 as f32).ceil() as u32,
+        //     1,
+        // ));
+        let nodes_in_level = nodes_per_level[octree_level as usize];
+        let groups_count = (nodes_in_level as f32 / CONFIG.working_group_size as f32).ceil() as u32;
+        self.shader.dispatch(groups_count);
         self.shader.wait();
 
-        let debug_values = helpers::get_values_from_texture_buffer(buffer, 10, 420_f32);
-        dbg!(&debug_values);
-
-        // helpers::bind_image_texture(0, textures.neighbors[2].0, gl::READ_ONLY, gl::R32UI);
-        // self.shader.set_uint(c_str!("axis"), 1);
+        helpers::bind_image_texture(0, textures.neighbors[2].0, gl::READ_ONLY, gl::R32UI);
+        self.shader.set_uint(c_str!("axis"), 1);
         // self.shader.dispatch_xyz(vec3(
         //     (CONFIG.viewport_width as f32 / 32 as f32).ceil() as u32,
         //     (CONFIG.viewport_height as f32 / 32 as f32).ceil() as u32,
         //     1,
         // ));
-        // self.shader.wait();
+        self.shader.dispatch(groups_count);
+        self.shader.wait();
 
-        // helpers::bind_image_texture(0, textures.neighbors[4].0, gl::READ_ONLY, gl::R32UI);
-        // self.shader.set_uint(c_str!("axis"), 2);
+        helpers::bind_image_texture(0, textures.neighbors[4].0, gl::READ_ONLY, gl::R32UI);
+        self.shader.set_uint(c_str!("axis"), 2);
         // self.shader.dispatch_xyz(vec3(
         //     (CONFIG.viewport_width as f32 / 32 as f32).ceil() as u32,
         //     (CONFIG.viewport_height as f32 / 32 as f32).ceil() as u32,
         //     1,
         // ));
-        // self.shader.wait();
+        self.shader.dispatch(groups_count);
+        self.shader.wait();
     }
 }
