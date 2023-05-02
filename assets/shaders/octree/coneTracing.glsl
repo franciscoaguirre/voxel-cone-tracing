89cd988b510b888f4ffffafc360f3bf545a25cee
Francisco Aguirre <franciscoaguirreperez@gmail.com>
2023-05-01 22:42:27 -0300

    Multiple cones

--- assets/shaders/octree/coneTracing.glsl
+++ assets/shaders/octree/coneTracing.glsl
@@ -63,8 +63,33 @@ void main() {
     vec3 position = (frag_position + vec3(1)) / float(2);
     // mat3 normalMatrix = mat3(transpose(inverse(view * model)));
     // vec3 normal = normalize(vec3(vec4(normalMatrix * frag_normal, 0)));
-    float AO = ambientOcclusion(position, frag_normal, 0.261799, 1.0); // 15deg as rad
+    vec3 direction = frag_normal;
+    vec3 helper = direction - vec3(0.1, 0, 0); // Random vector
+    vec3 tangent = normalize(helper - dot(direction, helper) * direction);
+    vec3 bitangent = cross(direction, tangent);
+    float AO;
+    AO += ambientOcclusion(position, direction, 0.261799, 1.0); // 15deg as rad
+
+    float angle = 1.0472;
+    float sinAngle = sin(angle);
+    float cosAngle = cos(angle);
+
+    direction = sinAngle * frag_normal + cosAngle * tangent;
+    AO += ambientOcclusion(position, direction, 0.261799, 1.0);
+
+    direction = sinAngle * frag_normal - cosAngle * tangent;
+    AO += ambientOcclusion(position, direction, 0.261799, 1.0);
+
+    direction = sinAngle * frag_normal + cosAngle * bitangent;
+    AO += ambientOcclusion(position, direction, 0.261799, 1.0);
+
+    direction = sinAngle * frag_normal - cosAngle * bitangent;
+    AO += ambientOcclusion(position, direction, 0.261799, 1.0);
     // float AO = ambientOcclusion(vec3(0.5, 0.5, 0.46), vec3(0, 0, 1), 0.261799, 1.0); // 15deg as rad
-    
-    FragColor = vec4(texture(texture_diffuse1, frag_textureCoordinates).xyz - vec3(AO), 1);
+
+    AO /= 5;
+
+    // FragColor = vec4(texture(texture_diffuse1, frag_textureCoordinates).xyz - vec3(AO), 1);
+    // FragColor = vec4(vec3(1) - vec3(AO), 1);
+    FragColor = texture(texture_diffuse1, frag_textureCoordinates);
 }
