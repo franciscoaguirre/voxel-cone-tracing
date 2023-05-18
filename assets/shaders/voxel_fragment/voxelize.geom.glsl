6e52c4767b7a9cd42fe20b4dcfbc861b0bd24fde
FelipePizzorno <68653470+felipe-pizzorno@users.noreply.github.com>
2023-05-18 20:30:41 -0300

    Test photon mapping (#64)
    * Prepare to test
    
    * Details
    
    * Minor improvements
    
    * Lol
    
    * Working conservative rasterization
    
    * Add z correction
    
    * Fix wrong node positions

--- assets/shaders/voxel_fragment/voxelize.geom.glsl
+++ assets/shaders/voxel_fragment/voxelize.geom.glsl
@@ -14,12 +14,18 @@ out VoxelData {
     vec3 normal;
     vec2 textureCoordinates;
 } Out;
+out vec2 edgeNormal;
+out vec2 semiDiagonal;
 
 flat out int frag_dominantAxis;
 flat out vec4 frag_aabb; 
 
 uniform int voxelDimension;
 uniform mat4 axisProjections[3];
+
+bool lineIntersection(vec2 p1, vec2 p2, vec2 q1, vec2 q2, out vec2 intersection);
+vec2 normalToSemiDiagonal(vec2 normal);
+float zFromPlaneAndPoint(vec2 point, vec4 plane, float defaultValue);
 
 // 0 means x, 1 means y, 2 means z
 int biggestComponent(vec3 triangleNormal) {
@@ -103,74 +109,96 @@ void main() {
     }
 
     vec3 projectedTriangleNormal = normalize(cross(vertex[1].xyz - vertex[0].xyz, vertex[2].xyz - vertex[0].xyz));
+    vec4 trianglePlane;
+    trianglePlane.xyz = projectedTriangleNormal;
+    trianglePlane.w = -dot(projectedTriangleNormal, vertex[0].xyz);
 
-    vec3 normals[3];
-    vec2 tex_coordinates[3];
-    // Change triangle winding, so normal points to "camera"
-    if (dot(projectedTriangleNormal, vec3(0.0, 0.0, 1.0)) < 0.0)
-    {
-        vec4 vertexTemp = vertex[2];
-        vertex[2] = vertex[1];
-        vertex[1] = vertexTemp;
-
-        normals[0] = In[0].normal;
-        normals[1] = In[2].normal;
-        normals[2] = In[1].normal;
-
-        tex_coordinates[0] = In[0].textureCoordinates;
-        tex_coordinates[1] = In[2].textureCoordinates;
-        tex_coordinates[2] = In[1].textureCoordinates;
+    float normalMultiplier = 1.0;
+    if (dot(projectedTriangleNormal, vec3(0, 0, 1)) > 0.0) {
+        normalMultiplier = -1.0;
     }
+
     // vec2(2.0 / voxelDimension) is the pixel size, as coordinates go from -1 to 1 (length 2), so a half pixel is half of that
     vec2 halfPixel = vec2(1.0 / voxelDimension);
+    // vec2 halfPixel = vec2(0.5);
 
     vec4 aabb = defineAabb(vertex, halfPixel);
     frag_aabb = aabb;
 
-    vec4 trianglePlane;
-
-    trianglePlane.xyz = normalize(cross(vertex[1].xyz - vertex[0].xyz, vertex[2].xyz - vertex[0].xyz));
-
-    trianglePlane.w = -dot(vertex[0].xyz, trianglePlane.xyz);
-
-    if (trianglePlane.z == 0.0) {
-        return;
-    }
-
-    vec3 plane[3];
-
-    for (int i = 0; i < gl_in.length(); i++) {
-      plane[i] = cross(vertex[i].xyw, vertex[(i + 2) % 3].xyw);
-      plane[i].z -= dot(halfPixel, abs(plane[i].xy));
-    }
-        
-    vec3 intersect[3];
-
-    for (int i = 0; i < gl_in.length(); i++) {
-        intersect[i] = cross(plane[i], plane[(i+1) % 3]);
-
-        if (intersect[i].z == 0.0) {
-            return;
-        }
-
-        intersect[i] /= intersect[i].z; 
-    }
-
+    vec3 expandedVertex[3];
     for (int i = 0; i < 3; i++) {
-      //gl_Position.xyw = intersect[i];
-      gl_Position = vertex[i];
+        vec2 currentEdge = vertex[(i + 1) % 3].xy - vertex[i].xy;
+        vec2 previousEdge = vertex[i].xy - vertex[(i + 2) % 3].xy;
+
+        vec2 currentNormal = normalize(vec2(-currentEdge.y, currentEdge.x)) * normalMultiplier;
+        vec2 currentSemiDiagonal = normalToSemiDiagonal(currentNormal);
+        vec2 previousNormal = normalize(vec2(-previousEdge.y, previousEdge.x)) * normalMultiplier;
+        vec2 previousSemiDiagonal = normalToSemiDiagonal(previousNormal);
+
+        vec2 currentExpanded1 = vertex[i].xy + currentSemiDiagonal * halfPixel;
+        vec2 currentExpanded2 = vertex[(i + 1) % 3].xy + currentSemiDiagonal * halfPixel;
+        vec2 previousExpanded1 = vertex[(i + 2) % 3].xy + previousSemiDiagonal * halfPixel;
+        vec2 previousExpanded2 = vertex[i].xy + previousSemiDiagonal * halfPixel;
+
+        vec2 intersection;
+        if (lineIntersection(currentExpanded1, currentExpanded2, previousExpanded1, previousExpanded2, intersection)) {
+            expandedVertex[i].xy = intersection;
+            expandedVertex[i].z = zFromPlaneAndPoint(intersection, trianglePlane, vertex[i].z);
+
+        } else {
+            // We f***** up
+            expandedVertex[i] = vertex[i].xyz;
+        }
         
-      // Calculate the new z-Coordinate derived from a point on a plane
-      //gl_Position.z = -(trianglePlane.x * intersect[i].x + trianglePlane.y * intersect[i].y + trianglePlane.w) / trianglePlane.z; 
-      //Out.position = intersect[i].xyz;
-      //Out.normal = normals[i];
-      //Out.textureCoordinates = tex_coordinates[i];
-      //gl_Position.z = -(trianglePlane.x * intersect[i].x + trianglePlane.y * intersect[i].y + trianglePlane.w) / trianglePlane.z; 
-      Out.position = vertex[i].xyz;
-      Out.normal = normals[i];
-      Out.textureCoordinates = tex_coordinates[i];
-      EmitVertex();
+        // Debug values
+        edgeNormal = currentNormal;
+        semiDiagonal = currentSemiDiagonal;
+
+        gl_Position = vec4(expandedVertex[i], 1.0);
+        Out.position = expandedVertex[i];
+        Out.normal = In[i].normal;
+        Out.textureCoordinates = In[i].textureCoordinates;
+        EmitVertex();
     }
 
     EndPrimitive();
+}
+
+vec2 normalToSemiDiagonal(vec2 normal) {
+    vec2 signVec = sign(normal);
+    vec2 semiDiagonal;
+
+    semiDiagonal.x = signVec.x == 0.0 ? 1.0 : signVec.x;
+    semiDiagonal.y = signVec.y == 0.0 ? 1.0 : signVec.y;
+    
+    return normalize(semiDiagonal);
+}
+
+bool lineIntersection(vec2 p1, vec2 p2, vec2 q1, vec2 q2, out vec2 intersection) {
+    vec2 r = p2 - p1;
+    vec2 s = q2 - q1;
+    float rxs = cross(vec3(r, 0.0), vec3(s, 0.0)).z;
+
+    if (abs(rxs) < 1e-6) {
+        // Lines are parallel or coincident
+        return false;
+    }
+
+    vec2 pq = q1 - p1;
+    float pqxr = cross(vec3(pq, 0.0), vec3(r, 0.0)).z;
+    float pqxs = cross(vec3(pq, 0.0), vec3(s, 0.0)).z;
+
+    float t = pqxs / rxs;
+    float u = pqxr / rxs;
+
+    intersection = p1 + t * r;
+
+    return true;
+}
+
+float zFromPlaneAndPoint(vec2 point, vec4 plane, float defaultValue) {
+  if (plane.z == 0.0) {
+    return defaultValue;
+  }
+  return (point.x * plane.x + point.y * plane.y + plane.w) / -plane.z;
 }
