01359ab3f9a4a9775214c8f9d3233ae79c9420da
Francisco Aguirre <franciscoaguirreperez@gmail.com>
2023-07-09 19:56:26 -0300

    Solving borderTransfer for level > 0 and (X, +)

--- src/octree/build/stages/border_transfer.rs
+++ src/octree/build/stages/border_transfer.rs
@@ -2,7 +2,7 @@ use c_str_macro::c_str;
 
 use crate::{
     config::CONFIG,
-    constants::Axis,
+    constants::{Axis, Direction, Sign},
     helpers,
     octree::{build::BrickPoolValues, NodeData, OctreeTextures},
     rendering::shader::Shader,
@@ -19,19 +19,29 @@ impl BorderTransferPass {
         }
     }
 
+    /// Runs the border transfer pass.
+    /// This runs border transfer for geometry and border nodes in sequence
+    /// so as to generate cohesive values.
     pub unsafe fn run(
         &self,
         textures: &OctreeTextures,
-        node_data: &NodeData,
+        geometry_node_data: &NodeData,
+        border_node_data: &NodeData,
         octree_level: u32,
         brick_pool_values: BrickPoolValues,
-        axis: Axis,
+        direction: Direction,
     ) {
         self.shader.use_program();
 
         self.shader.set_uint(c_str!("octreeLevel"), octree_level);
         self.shader
             .set_uint(c_str!("voxelDimension"), CONFIG.voxel_dimension);
+
+        // We want the direction to be the same for both runs (geometry and border)
+        self.shader
+            .set_int(c_str!("direction.axis"), direction.axis.into());
+        self.shader
+            .set_int(c_str!("direction.sign"), direction.sign.into());
 
         match brick_pool_values {
             BrickPoolValues::Colors => helpers::bind_3d_image_texture(
@@ -47,18 +57,77 @@ impl BorderTransferPass {
                 gl::RGBA8,
             ),
         }
-        helpers::bind_image_texture(2, node_data.level_start_indices.0, gl::READ_ONLY, gl::R32UI);
-
-        let nodes_in_level = node_data.nodes_per_level[octree_level as usize];
-        let groups_count = (nodes_in_level as f32 / CONFIG.working_group_size as f32).ceil() as u32;
-
-        self.shader.set_uint(c_str!("axis"), axis.into());
-        let neighbors = match axis {
-            Axis::X => textures.neighbors[0].0,
-            Axis::Y => textures.neighbors[2].0,
-            Axis::Z => textures.neighbors[4].0,
+        let other_axis = if direction.axis == Axis::X {
+            vec![Axis::Y, Axis::Z]
+        } else if direction.axis == Axis::Y {
+            vec![Axis::X, Axis::Z]
+        } else {
+            vec![Axis::X, Axis::Y]
         };
-        helpers::bind_image_texture(0, neighbors, gl::READ_ONLY, gl::R32UI);
+
+        // First run for geometry nodes
+        helpers::bind_image_texture(
+            2,
+            geometry_node_data.level_start_indices.0,
+            gl::READ_ONLY,
+            gl::R32UI,
+        );
+        let nodes_in_level = geometry_node_data.nodes_per_level[octree_level as usize];
+        let groups_count = (nodes_in_level as f32 / CONFIG.working_group_size as f32).ceil() as u32;
+        for axis in other_axis.iter() {
+            self.shader.set_uint(c_str!("axis"), (*axis).into());
+            let mut neighbors_texture_number = match axis {
+                Axis::X => 0,
+                Axis::Y => 2,
+                Axis::Z => 4,
+            };
+            neighbors_texture_number = match direction.sign {
+                Sign::Pos => neighbors_texture_number,
+                Sign::Neg => neighbors_texture_number + 1,
+            };
+            helpers::bind_image_texture(
+                0,
+                textures.neighbors[neighbors_texture_number].0,
+                gl::READ_ONLY,
+                gl::R32UI,
+            );
+            self.shader.dispatch(groups_count);
+            self.shader.wait();
+        }
+        self.shader.set_uint(c_str!("axis"), direction.axis.into());
+        self.shader.dispatch(groups_count);
+        self.shader.wait();
+
+        // Second run for border nodes
+        helpers::bind_image_texture(
+            2,
+            border_node_data.level_start_indices.0,
+            gl::READ_ONLY,
+            gl::R32UI,
+        );
+        let nodes_in_level = border_node_data.nodes_per_level[octree_level as usize];
+        let groups_count = (nodes_in_level as f32 / CONFIG.working_group_size as f32).ceil() as u32;
+        for axis in other_axis.iter() {
+            self.shader.set_uint(c_str!("axis"), (*axis).into());
+            let mut neighbors_texture_number = match axis {
+                Axis::X => 0,
+                Axis::Y => 2,
+                Axis::Z => 4,
+            };
+            neighbors_texture_number = match direction.sign {
+                Sign::Pos => neighbors_texture_number,
+                Sign::Neg => neighbors_texture_number + 1,
+            };
+            helpers::bind_image_texture(
+                0,
+                textures.neighbors[neighbors_texture_number].0,
+                gl::READ_ONLY,
+                gl::R32UI,
+            );
+            self.shader.dispatch(groups_count);
+            self.shader.wait();
+        }
+        self.shader.set_uint(c_str!("axis"), direction.axis.into());
         self.shader.dispatch(groups_count);
         self.shader.wait();
     }
