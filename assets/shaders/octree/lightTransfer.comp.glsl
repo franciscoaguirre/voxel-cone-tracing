4a4d0e820a682bdd53e255aff6dcc7237370f86e
Felipe Pizzorno <felipepizzorno98@gmail.com>
2023-08-05 16:29:53 -0300

    Revert light transfer change

--- assets/shaders/octree/lightTransfer.comp.glsl
+++ assets/shaders/octree/lightTransfer.comp.glsl
@@ -17,9 +17,7 @@ uniform uint octreeLevel;
 uniform uint voxelDimension;
 
 #include "./_threadNodeUtil.glsl"
-#include "./_traversalHelpers.glsl"
-#include "./_octreeTraversal.glsl"
-#include "./_brickCoordinates.glsl"
+#include "assets/shaders/octree/_brickCoordinates.glsl"
 
 // Doing it this way can have concurrency problems if we run all three axis concurrently (not sure if posible, but paper does it in two passes)
 
