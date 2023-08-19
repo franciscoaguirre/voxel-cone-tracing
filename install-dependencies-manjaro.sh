1d5ac5dbb4f5164f4019455f5284e513cd34ec22
Felipe Pizzorno <felipepizzorno98@gmail.com>
2023-08-19 15:51:15 -0300

    Add script for installing manjaro dependencies

---
+++ install-dependencies-manjaro.sh
@@ -0,0 +1,9 @@
+# Converted with chatgpt from the ubuntu script
+sudo pacman -S base-devel
+sudo pacman -S cmake
+sudo pacman -S glfw-x11
+sudo pacman -S glfw-x11
+sudo pacman -S libxinerama
+sudo pacman -S libxcursor
+sudo pacman -S libxi
+sudo pacman -S pkgconf alsa-lib openssl cmake freetype2 expat
