36f5cf110b368a21fbfc19ec434b486bc230740b
Francisco Aguirre <franciscoaguirreperez@gmail.com>
2023-08-27 00:14:59 -0300

    Add color sampling code to debug cone

--- src/cone_tracing/debug_cone.rs
+++ src/cone_tracing/debug_cone.rs
@@ -1,7 +1,10 @@
 use std::collections::HashSet;
+use std::fmt;
 
 use c_str_macro::c_str;
-use cgmath::{vec3, Matrix4, Vector3};
+use cgmath::{point3, vec3, Matrix4, Vector3};
+use colored::{customcolors, Colorize};
+
 use gl::types::GLuint;
 
 use crate::{
@@ -24,8 +27,32 @@ pub struct DebugCone {
     previous_values: HashSet<u32>,
     nodes_queried: BufferTexture,
     nodes_queried_counter: GLuint,
-    sampled_color_texture: BufferTexture,
+    sampled_colors_texture: BufferTexture,
     vao: GLuint,
+}
+
+pub struct VoxelData {
+    color: (f32, f32, f32, f32),
+    octree_level: f32,
+}
+impl fmt::Display for VoxelData {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let color = customcolors::CustomColor {
+            r: (self.color.0 * 255.0) as u8,
+            g: (self.color.1 * 255.0) as u8,
+            b: (self.color.2 * 255.0) as u8,
+        };
+        write!(
+            f,
+            "{} {}, octree_level: {}",
+            "color:".custom_color(color),
+            format!(
+                "({}, {}, {}, {})",
+                self.color.0, self.color.1, self.color.2, self.color.3
+            ),
+            self.octree_level,
+        )
+    }
 }
 
 impl DebugCone {
@@ -35,23 +62,30 @@ impl DebugCone {
 
         let mut transform = Transform::default();
         transform.movement_speed = 0.25;
+        transform.position = point3(0.5, 0.5, 0.43);
 
         Self {
             shader: compile_shaders!("assets/shaders/debug/debugConeTracing.glsl", debug = true),
             transform,
             direction: vec3(0.0, 0.0, 1.0),
             previous_values: HashSet::new(),
-            nodes_queried: helpers::generate_texture_buffer4(1000, gl::R32UI, 69u32, gl::DYNAMIC_READ),
-            sampled_color_texture: helpers::generate_texture_buffer4(5, gl::R32F, 69f32, gl::DYNAMIC_READ),
+            nodes_queried: helpers::generate_texture_buffer4(
+                1000,
+                gl::R32UI,
+                69u32,
+                gl::DYNAMIC_READ,
+            ),
+            sampled_colors_texture: helpers::generate_texture_buffer4(
+                100,
+                gl::R32F,
+                69f32,
+                gl::DYNAMIC_READ,
+            ),
             nodes_queried_counter: helpers::generate_atomic_counter_buffer1(gl::DYNAMIC_READ),
             cone_angle: 0.263599,
             vao,
         }
     }
-
-    // self.transform.position.x = 0.5;
-    // self.transform.position.y = 0.5;
-    // self.transform.position.z = 0.43;
 
     pub unsafe fn run(
         &mut self,
@@ -60,33 +94,99 @@ impl DebugCone {
         view: &Matrix4<f32>,
         selected_debug_nodes: &mut Vec<DebugNode>,
     ) {
+        helpers::clear_texture_buffer(self.sampled_colors_texture.1, 100, 42f32, gl::DYNAMIC_READ);
         self.shader.use_program();
 
         gl::BindVertexArray(self.vao);
 
         helpers::bind_image_texture(0, self.nodes_queried.0, gl::WRITE_ONLY, gl::R32UI);
         helpers::bind_image_texture(1, textures.node_pool.0, gl::READ_ONLY, gl::R32UI);
-        helpers::bind_image_texture(2, self.sampled_color_texture.0, gl::WRITE_ONLY, gl::R32F);
+        helpers::bind_image_texture(2, self.sampled_colors_texture.0, gl::WRITE_ONLY, gl::R32F);
 
         gl::BindBufferBase(gl::ATOMIC_COUNTER_BUFFER, 0, self.nodes_queried_counter);
 
-        gl::ActiveTexture(gl::TEXTURE0);
-        gl::BindTexture(gl::TEXTURE_3D, textures.brick_pool_colors[0]);
-        self.shader.set_int(c_str!("brickPoolColors"), 0 as i32);
-        gl::TexParameteri(gl::TEXTURE_3D, gl::TEXTURE_WRAP_S, gl::CLAMP_TO_EDGE as i32);
-        gl::TexParameteri(gl::TEXTURE_3D, gl::TEXTURE_WRAP_T, gl::CLAMP_TO_EDGE as i32);
-        gl::TexParameteri(gl::TEXTURE_3D, gl::TEXTURE_WRAP_R, gl::CLAMP_TO_EDGE as i32);
-        gl::TexParameteri(gl::TEXTURE_3D, gl::TEXTURE_MIN_FILTER, gl::LINEAR as i32);
-        gl::TexParameteri(gl::TEXTURE_3D, gl::TEXTURE_MAG_FILTER, gl::LINEAR as i32);
+        let brick_pool_textures = vec![
+            (
+                c_str!("brickPoolColorsX"),
+                textures.brick_pool_colors[0],
+                gl::LINEAR as i32,
+            ),
+            (
+                c_str!("brickPoolColorsXNeg"),
+                textures.brick_pool_colors[1],
+                gl::LINEAR as i32,
+            ),
+            (
+                c_str!("brickPoolColorsY"),
+                textures.brick_pool_colors[2],
+                gl::LINEAR as i32,
+            ),
+            (
+                c_str!("brickPoolColorsYNeg"),
+                textures.brick_pool_colors[3],
+                gl::LINEAR as i32,
+            ),
+            (
+                c_str!("brickPoolColorsZ"),
+                textures.brick_pool_colors[4],
+                gl::LINEAR as i32,
+            ),
+            (
+                c_str!("brickPoolColorsZNeg"),
+                textures.brick_pool_colors[5],
+                gl::LINEAR as i32,
+            ),
+            (
+                c_str!("brickPoolNormals"),
+                textures.brick_pool_normals,
+                gl::NEAREST as i32,
+            ),
+            // Irradiance textures
+            (
+                c_str!("brickPoolIrradianceX"),
+                textures.brick_pool_irradiance[0],
+                gl::LINEAR as i32,
+            ),
+            (
+                c_str!("brickPoolIrradianceXNeg"),
+                textures.brick_pool_irradiance[1],
+                gl::LINEAR as i32,
+            ),
+            (
+                c_str!("brickPoolIrradianceY"),
+                textures.brick_pool_irradiance[2],
+                gl::LINEAR as i32,
+            ),
+            (
+                c_str!("brickPoolIrradianceYNeg"),
+                textures.brick_pool_irradiance[3],
+                gl::LINEAR as i32,
+            ),
+            (
+                c_str!("brickPoolIrradianceZ"),
+                textures.brick_pool_irradiance[4],
+                gl::LINEAR as i32,
+            ),
+            (
+                c_str!("brickPoolIrradianceZNeg"),
+                textures.brick_pool_irradiance[5],
+                gl::LINEAR as i32,
+            ),
+        ];
 
-        gl::ActiveTexture(gl::TEXTURE1);
-        gl::BindTexture(gl::TEXTURE_3D, textures.brick_pool_photons);
-        self.shader.set_int(c_str!("brickPoolPhotons"), 1 as i32);
-        gl::TexParameteri(gl::TEXTURE_3D, gl::TEXTURE_WRAP_S, gl::CLAMP_TO_EDGE as i32);
-        gl::TexParameteri(gl::TEXTURE_3D, gl::TEXTURE_WRAP_T, gl::CLAMP_TO_EDGE as i32);
-        gl::TexParameteri(gl::TEXTURE_3D, gl::TEXTURE_WRAP_R, gl::CLAMP_TO_EDGE as i32);
-        gl::TexParameteri(gl::TEXTURE_3D, gl::TEXTURE_MIN_FILTER, gl::NEAREST as i32);
-        gl::TexParameteri(gl::TEXTURE_3D, gl::TEXTURE_MAG_FILTER, gl::NEAREST as i32);
+        let mut texture_counter = 0;
+
+        for &(texture_name, texture, sample_interpolation) in brick_pool_textures.iter() {
+            gl::ActiveTexture(gl::TEXTURE0 + texture_counter);
+            gl::BindTexture(gl::TEXTURE_3D, texture);
+            self.shader.set_int(texture_name, texture_counter as i32);
+            gl::TexParameteri(gl::TEXTURE_3D, gl::TEXTURE_WRAP_S, gl::CLAMP_TO_EDGE as i32);
+            gl::TexParameteri(gl::TEXTURE_3D, gl::TEXTURE_WRAP_T, gl::CLAMP_TO_EDGE as i32);
+            gl::TexParameteri(gl::TEXTURE_3D, gl::TEXTURE_WRAP_R, gl::CLAMP_TO_EDGE as i32);
+            gl::TexParameteri(gl::TEXTURE_3D, gl::TEXTURE_MIN_FILTER, sample_interpolation);
+            gl::TexParameteri(gl::TEXTURE_3D, gl::TEXTURE_MAG_FILTER, sample_interpolation);
+            texture_counter += 1;
+        }
 
         self.shader
             .set_uint(c_str!("voxelDimension"), CONFIG.voxel_dimension);
@@ -113,12 +213,14 @@ impl DebugCone {
         gl::DrawArrays(gl::POINTS, 0, 1);
 
         let values = helpers::get_values_from_texture_buffer(self.nodes_queried.1, 1000, 42u32);
-        let sampled_color = helpers::get_values_from_texture_buffer(self.sampled_color_texture.1, 5, 32f32);
-        dbg!(sampled_color);
+        let sampled_colors =
+            helpers::get_values_from_texture_buffer(self.sampled_colors_texture.1, 100, 32f32);
+        dbg!(&sampled_colors[0..5]);
+        pretty_print_data(&sampled_colors[5..]);
 
-        let values_set = HashSet::from_iter(values.iter().cloned());
         let total_nodes_queried =
             helpers::get_value_from_atomic_counter(self.nodes_queried_counter) as usize;
+        let values_set = HashSet::from_iter(values[..total_nodes_queried].iter().cloned());
 
         if self.previous_values != values_set {
             dbg!(&values[..total_nodes_queried]);
@@ -131,5 +233,19 @@ impl DebugCone {
         }
 
         gl::BindVertexArray(0);
+    }
+}
+
+const AMOUNT_OF_VALUES: usize = 5;
+
+fn pretty_print_data(sampled_colors: &[f32]) {
+    for chunk in sampled_colors.chunks_exact(AMOUNT_OF_VALUES) {
+        let voxel_data = VoxelData {
+            color: (chunk[0], chunk[1], chunk[2], chunk[3]),
+            octree_level: chunk[4],
+        };
+        if voxel_data.octree_level != 42.0 {
+            println!("{}", voxel_data);
+        }
     }
 }
