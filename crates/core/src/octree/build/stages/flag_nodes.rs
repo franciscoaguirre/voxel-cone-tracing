ea30606e8b83ba6c59dc8f7d20d14dc40e5772ff
Francisco Aguirre <franciscoaguirreperez@gmail.com>
2023-10-16 19:05:51 -0300

    Flag nodes test skeleton

--- crates/core/src/octree/build/stages/flag_nodes.rs
+++ crates/core/src/octree/build/stages/flag_nodes.rs
@@ -60,6 +60,42 @@ mod tests {
     use std::path::PathBuf;
     use std::env;
 
+    /// The idea is to generate a testing node pool we can use to test the SVO construction,
+    /// the SVO traversal, neighbors, etc.
+    unsafe fn get_node_pool() -> BufferTexture {
+        let data: Vec<u32> = vec![
+            // Level 0
+            0, 0, 0, 0, 0, 0, 0, 0,
+            // Level 1
+            0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0,
+        ];
+        helpers::generate_texture_buffer_with_initial_data(8, gl::R32UI, data)
+    }
+
+    /// The idea is to generate a testing voxel fragment list we can use to feed the SVO construction
+    unsafe fn get_voxel_data() -> VoxelData {
+        VoxelData {
+            voxel_positions: helpers::generate_texture_buffer_with_initial_data(
+                1,
+                gl::RGB10_A2UI,
+                vec![
+                    // TODO: Add useful testing voxels, probably very simple coordinates
+                    134461564_u32, // (124, 238, 128)
+                ],
+            ),
+            number_of_voxel_fragments: 1,
+            voxel_colors: (0, 0), // Irrelevant
+            voxel_normals: (0, 0), // Irrelevant
+        }        
+    }
+
     #[test]
     fn flag_nodes_works() {
         let (_glfw, _window) = test_utils::init_opengl_context();
@@ -70,28 +106,36 @@ mod tests {
         path.pop();
         env::set_current_dir(path).unwrap();
 
+        // TODO: Make this small enough to understand,
+        // but good enough to test what we want.
+        let voxel_dimension_exponent = 1;
+
+        let flag_value = 1 << 31;
+
         unsafe {
-            Config::initialize(Config::new(1));
+            // Initialize everything
+            Config::initialize(Config::new(voxel_dimension_exponent));
             let flag_nodes_pass = FlagNodesPass::init();
-            let voxel_data = VoxelData {
-                voxel_positions: helpers::generate_texture_buffer_with_initial_data(
-                    1,
-                    gl::RGB10_A2UI,
-                    vec![
-                        134461564_u32,
-                    ],
-                ),
-                number_of_voxel_fragments: 1,
-                voxel_colors: (0, 0), // Irrelevant
-                voxel_normals: (0, 0), // Irrelevant
-            };
-            let node_pool = helpers::generate_texture_buffer(8, gl::R32UI, 0_u32);
+            let voxel_data = get_voxel_data();
+            let node_pool = get_node_pool();
+            // TODO: Test with multiple levels,
+            // we should be able to test this independently from `allocate_nodes`.
+            // Although after both are tested, we should write an integration test
+            // for the both in sequence.
             let input = FlagNodesInput {
                 octree_level: 0,
                 voxel_data: &voxel_data,
                 node_pool,
             };
+
+            // Run the shader
             flag_nodes_pass.run(input);
+
+            // Verify output
+            // TODO: We only verify the first child pointer in the first node is flagged.
+            // We should verify more once we have a reasonable voxel fragment list to test with.
+            let output = helpers::get_values_from_texture_buffer(node_pool.1, 8, 42_u32);
+            assert_eq!(output[0], flag_value);
         }
     }
 }
