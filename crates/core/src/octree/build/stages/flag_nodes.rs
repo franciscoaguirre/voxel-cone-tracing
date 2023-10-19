a060c13b3981e1d4684c0c3cda9e0459b3e13a5e
Felipe Pizzorno <felipepizzorno98@gmail.com>
2023-10-18 22:31:54 -0300

    Add test cases, and descriptions

--- crates/core/src/octree/build/stages/flag_nodes.rs
+++ crates/core/src/octree/build/stages/flag_nodes.rs
@@ -64,6 +64,7 @@ mod tests {
     struct TestCase {
         pub input: FlagNodesInput,
         pub expected_output: Vec<u32>,
+        pub description: String,
     }
 
     #[test]
@@ -78,97 +79,221 @@ mod tests {
 
         // TODO: Make this small enough to understand,
         // but good enough to test what we want.
-        let voxel_dimension_exponent = 3;
+        let voxel_dimension_exponent = 4;
 
         // Flag left in node's child pointers to notify the child they point to
         // should be allocated, in `allocate_nodes`
         #[allow(non_snake_case)]
-        let F = 1 << 31;
 
         unsafe {
-            let test_data = vec![
-                TestCase {
-                    input: FlagNodesInput {
-                        octree_level: 0,
-                        voxel_data: BufferTextureV2::from_data(vec![
-                            helpers::rgb10_a2ui_to_r32ui(0, 0, 0),
-                        ]).into(),
-                        node_pool: BufferTextureV2::from_data(vec![
-                            // Level 0
-                            0, 0, 0, 0, 0, 0, 0, 0,
-                            // Level 1
-                            0, 0, 0, 0, 0, 0, 0, 0,
-                            0, 0, 0, 0, 0, 0, 0, 0,
-                            0, 0, 0, 0, 0, 0, 0, 0,
-                            0, 0, 0, 0, 0, 0, 0, 0,
-                            0, 0, 0, 0, 0, 0, 0, 0,
-                            0, 0, 0, 0, 0, 0, 0, 0,
-                            0, 0, 0, 0, 0, 0, 0, 0,
-                            0, 0, 0, 0, 0, 0, 0, 0,
-                        ]),
-                    },
-                    expected_output: vec![
-                        // Level 0
-                        F, 0, 0, 0, 0, 0, 0, 0,
-                        // Level 1
-                        0, 0, 0, 0, 0, 0, 0, 0,
-                        0, 0, 0, 0, 0, 0, 0, 0,
-                        0, 0, 0, 0, 0, 0, 0, 0,
-                        0, 0, 0, 0, 0, 0, 0, 0,
-                        0, 0, 0, 0, 0, 0, 0, 0,
-                        0, 0, 0, 0, 0, 0, 0, 0,
-                        0, 0, 0, 0, 0, 0, 0, 0,
-                        0, 0, 0, 0, 0, 0, 0, 0,
-                    ],
-                },
-                TestCase {
-                    input: FlagNodesInput {
-                        octree_level: 1,
-                        voxel_data: BufferTextureV2::from_data(vec![
-                            helpers::rgb10_a2ui_to_r32ui(0, 0, 0),
-                        ]).into(),
-                        node_pool: BufferTextureV2::from_data(vec![
-                            // Level 0
-                            1, 0, 0, 0, 0, 0, 0, 0,
-                            // Level 1
-                            0, 0, 0, 0, 0, 0, 0, 0,
-                            0, 0, 0, 0, 0, 0, 0, 0,
-                            0, 0, 0, 0, 0, 0, 0, 0,
-                            0, 0, 0, 0, 0, 0, 0, 0,
-                            0, 0, 0, 0, 0, 0, 0, 0,
-                            0, 0, 0, 0, 0, 0, 0, 0,
-                            0, 0, 0, 0, 0, 0, 0, 0,
-                            0, 0, 0, 0, 0, 0, 0, 0,
-                        ]),
-                    },
-                    expected_output: vec![
-                        // Level 0
-                        1, 0, 0, 0, 0, 0, 0, 0,
-                        // Level 1
-                        F, 0, 0, 0, 0, 0, 0, 0,
-                        0, 0, 0, 0, 0, 0, 0, 0,
-                        0, 0, 0, 0, 0, 0, 0, 0,
-                        0, 0, 0, 0, 0, 0, 0, 0,
-                        0, 0, 0, 0, 0, 0, 0, 0,
-                        0, 0, 0, 0, 0, 0, 0, 0,
-                        0, 0, 0, 0, 0, 0, 0, 0,
-                        0, 0, 0, 0, 0, 0, 0, 0,
-                    ],
-                },
-            ];
+            let test_data = test_cases();
 
             // Initialize everything
             Config::initialize(Config::new(voxel_dimension_exponent));
             let flag_nodes_pass = FlagNodesPass::init();
 
-            for TestCase { input, expected_output } in test_data.iter() {
+            for TestCase { input, expected_output, description } in test_data.iter() {
                 // Run the shader
                 flag_nodes_pass.run(input.clone());
 
                 // Verify output
                 let output = input.node_pool.data();
+                dbg!(description);
                 assert_eq!(output, *expected_output);
+                dbg!("Passed");
             }
         }
+    }
+
+    unsafe fn test_cases() -> std::vec::Vec<TestCase> {
+        let F = 1 << 31;
+        vec![
+            TestCase {
+                description: String::from("Base case, empty node pool, voxel_position on initial coordinate"),
+                input: FlagNodesInput {
+                    octree_level: 0,
+                    voxel_data: BufferTextureV2::from_data(vec![
+                       helpers::rgb10_a2ui_to_r32ui(0, 0, 0),
+                    ]).into(),
+                    node_pool: BufferTextureV2::from_data(vec![
+                      // Level 0
+                      0, 0, 0, 0, 0, 0, 0, 0,
+                      // Level 1
+                      0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0,
+                    ]),
+                },
+                expected_output: vec![
+                    // Level 0
+                    F, 0, 0, 0, 0, 0, 0, 0,
+                    // Level 1
+                    0, 0, 0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0, 0, 0, 0,
+                ],
+            },
+            TestCase {
+                description: String::from("First level already complete, flag subnode for x = 1, z = y = 0"),
+                input: FlagNodesInput {
+                    octree_level: 1,
+                    voxel_data: BufferTextureV2::from_data(vec![
+                       helpers::rgb10_a2ui_to_r32ui(4, 2, 0),
+                    ]).into(),
+                    node_pool: BufferTextureV2::from_data(vec![
+                      // Level 0
+                      1, 0, 0, 0, 0, 0, 0, 0,
+                      // Level 1
+                      0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0,
+                    ]),
+                },
+                expected_output: vec![
+                    // Level 0
+                    1, 0, 0, 0, 0, 0, 0, 0,
+                    // Level 1
+                    0, F, 0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0, 0, 0, 0,
+                ],
+            },
+            TestCase {
+                description: String::from("First level already complete, flag subnode for x = y = z = 1"),
+                input: FlagNodesInput {
+                    octree_level: 1,
+                    voxel_data: BufferTextureV2::from_data(vec![
+                       helpers::rgb10_a2ui_to_r32ui(4, 4, 4),
+                    ]).into(),
+                    node_pool: BufferTextureV2::from_data(vec![
+                      // Level 0
+                      1, 0, 0, 0, 0, 0, 0, 0,
+                      // Level 1
+                      0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0,
+                    ]),
+                },
+                expected_output: vec![
+                    // Level 0
+                    1, 0, 0, 0, 0, 0, 0, 0,
+                    // Level 1
+                    0, 0, 0, 0, 0, 0, 0, F,
+                    0, 0, 0, 0, 0, 0, 0, 0,
+                ],
+            },
+            TestCase {
+                description: String::from("First two levels already complete, flag subnode for x = 0, z = y = 1"),
+                input: FlagNodesInput {
+                    octree_level: 1,
+                    voxel_data: BufferTextureV2::from_data(vec![
+                       helpers::rgb10_a2ui_to_r32ui(4, 6, 6),
+                    ]).into(),
+                    node_pool: BufferTextureV2::from_data(vec![
+                      // Level 0
+                      1, 0, 0, 0, 0, 0, 0, 0,
+                      // Level 1
+                      0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0,
+                    ]),
+                },
+                expected_output: vec![
+                    // Level 0
+                    1, 0, 0, 0, 0, 0, 0, 0,
+                    // Level 1
+                    0, 0, 0, 0, 0, 0, 0, 2,
+                    // Level 2
+                    0, 0, 0, 0, 0, 0, F, 0,
+                ],
+            },
+            TestCase {
+                description: String::from("Empty node pool, multiple voxel positions"),
+                input: FlagNodesInput {
+                    octree_level: 0,
+                    voxel_data: BufferTextureV2::from_data(vec![
+                       helpers::rgb10_a2ui_to_r32ui(0, 0, 0),
+                       helpers::rgb10_a2ui_to_r32ui(8, 10, 6),
+                       helpers::rgb10_a2ui_to_r32ui(8, 7, 10),
+                       helpers::rgb10_a2ui_to_r32ui(8, 6, 10),
+                    ]).into(),
+                    node_pool: BufferTextureV2::from_data(vec![
+                      // Level 0
+                      0, 0, 0, 0, 0, 0, 0, 0,
+                      // Level 1
+                      0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0,
+                    ]),
+                },
+                expected_output: vec![
+                    // Level 0
+                    F, 0, 0, F, 0, F, 0, 0,
+                    // Level 1
+                    0, 0, 0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0, 0, 0, 0,
+                ],
+            },
+            TestCase {
+                description: String::from("First level already complete, flag many subnodes"),
+                input: FlagNodesInput {
+                    octree_level: 1,
+                    voxel_data: BufferTextureV2::from_data(vec![
+                       helpers::rgb10_a2ui_to_r32ui(0, 0, 0),
+                       helpers::rgb10_a2ui_to_r32ui(8, 10, 6),
+                       helpers::rgb10_a2ui_to_r32ui(8, 7, 10),
+                       helpers::rgb10_a2ui_to_r32ui(8, 6, 10),
+                    ]).into(),
+                    node_pool: BufferTextureV2::from_data(vec![
+                      // Level 0
+                      1, 0, 0, 2, 0, 3, 0, 0,
+                      // Level 1
+                      0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0,
+                      // Level 2
+                    ]),
+                },
+                expected_output: vec![
+                    // Level 0
+                    1, 0, 0, 2, 0, 3, 0, 0,
+                    // Level 1
+                    F, 0, 0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 0, F, 0, 0, 0,
+                    0, 0, F, 0, 0, 0, 0, 0,
+                    // Level 2
+                    0, 0, 0, 0, 0, 0, 0, 0,
+                ],
+            },
+            TestCase {
+                description: String::from("First two levels already complete, flag many subnodes"),
+                input: FlagNodesInput {
+                    octree_level: 2,
+                    voxel_data: BufferTextureV2::from_data(vec![
+                       helpers::rgb10_a2ui_to_r32ui(0, 0, 0),
+                       helpers::rgb10_a2ui_to_r32ui(8, 10, 6),
+                       helpers::rgb10_a2ui_to_r32ui(8, 7, 10),
+                       helpers::rgb10_a2ui_to_r32ui(8, 7, 9),
+                    ]).into(),
+                    node_pool: BufferTextureV2::from_data(vec![
+                        // Level 0
+                        1, 0, 0, 2, 0, 3, 0, 0,
+                        // Level 1
+                        4, 0, 0, 0, 0, 0, 0, 0,
+                        0, 0, 0, 0, 5, 0, 0, 0,
+                        0, 0, 6, 0, 0, 0, 0, 0,
+                        // Level 2
+                        0, 0, 0, 0, 0, 0, 0, 0,
+                        0, 0, 0, 0, 0, 0, 0, 0,
+                        0, 0, 0, 0, 0, 0, 0, 0,
+                    ]),
+                },
+                expected_output: vec![
+                    // Level 0
+                    1, 0, 0, 2, 0, 3, 0, 0,
+                    // Level 1
+                    4, 0, 0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0, 5, 0, 0,
+                    0, 0, 6, 0, 0, 0, 0, 0,
+                    // Level 2
+                    F, 0, 0, 0, 0, 0, 0, 0,
+                    0, 0, F, 0, 0, 0, 0, 0,
+                    0, 0, F, 0, 0, 0, F, 0,
+                ],
+            },
+        ]
     }
 }
