52f0118acb5bdc3fb20d77f016c52a9650775bda
Francisco Aguirre <franciscoaguirreperez@gmail.com>
2023-10-22 19:43:04 +0200

    Add color to the voxels of the cornell box (#96)

--- crates/engine/src/material.rs
+++ crates/engine/src/material.rs
@@ -25,6 +25,7 @@ pub struct MaterialProperties {
 
 impl MaterialProperties {
     pub unsafe fn set_uniforms(&self, shader: &Shader) {
+        shader.set_bool(c_str!("hasMaterial"), true);
         shader.set_vec3(c_str!("material.color"), self.color.x, self.color.y, self.color.z);
         shader.set_float(c_str!("material.diffuse"), self.diffuse);
         shader.set_float(c_str!("material.specular"), self.specular);
