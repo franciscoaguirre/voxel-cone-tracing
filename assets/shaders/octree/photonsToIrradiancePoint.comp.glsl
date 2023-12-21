38597b6122999376b27330fad73fc0ca0c3fcd1e
Francisco Aguirre <franciscoaguirreperez@gmail.com>
2023-12-20 23:08:08 -0300

    Add attenuation, not working

--- assets/shaders/octree/photonsToIrradiancePoint.comp.glsl
+++ assets/shaders/octree/photonsToIrradiancePoint.comp.glsl
@@ -14,11 +14,16 @@ uniform usampler2DArray lightViewMap;
 uniform uint voxelDimension;
 uniform uint octreeLevel;
 uniform float lightIntensity;
+uniform vec3 lightPosition;
 
 #include "./_helpers.glsl"
 #include "./_traversalHelpers.glsl"
 #include "./_octreeTraversal.glsl"
 #include "./_brickCoordinates.glsl"
+
+vec3 toVoxelSpace(vec3 positionWorldSpace) {
+  return (positionWorldSpace + vec3(1)) / 2.0;
+}
 
 void main() {
     uvec3 queryCoordinates = texelFetch(
@@ -50,12 +55,18 @@ void main() {
     vec4 voxelColor = texelFetch(brickPoolColors, brickCoordinates + brickOffset, 0);
     uint numberOfPhotons = texelFetch(brickPoolPhotons, brickCoordinates + brickOffset, 0).r;
 
+    float lightDistance = length(toVoxelSpace(lightPosition) - normalizedQueryCoordinates);
+    float c1 = 1.0;
+    float c2 = 0.09;
+    float c3 = 0.032;
+    float attenuation = c1 + c2 * lightDistance + c3 * lightDistance * lightDistance;
+
     // TODO: Use also total photon hits here for the multiplier.
     // Every octree level added separates the current surface touched by photons in
     // 4 (2D section of a voxel is separated in 4 new voxels, each with a fourth of the amount of photons)
     // float multiplier = numberOfPhotons * pow(4, octreeLevel) / float(262144); 
     // float multiplier = clamp(float(numberOfPhotons), 0.0, 1.0);
-    vec4 irradiance = vec4(voxelColor.xyz * numberOfPhotons * lightIntensity, 1.0);
+    vec4 irradiance = vec4(voxelColor.xyz * numberOfPhotons * lightIntensity / attenuation, 1.0);
 
     imageStore(brickPoolIrradiance, brickCoordinates + brickOffset, irradiance);
 }
