aa0d43fc6a570f0eed57a30f989d3c66207c790c
Francisco Aguirre <franciscoaguirreperez@gmail.com>
2024-12-15 19:48:13 -0300

    chore: add thousands separator to number of voxel fragments log

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -149,7 +149,17 @@ fn run_application(parameters: ApplicationParameters, mut glfw: Glfw) {
 
     let (voxel_positions, number_of_voxel_fragments, voxel_colors, voxel_normals) =
         unsafe { voxelization::build_voxel_fragment_list(&mut objects[..], &scene_aabb) };
-    info!("Number of voxel fragments: {}", number_of_voxel_fragments);
+    info!(
+        "Number of voxel fragments: {}",
+        number_of_voxel_fragments.to_string()
+            .as_bytes()
+            .rchunks(3)
+            .rev()
+            .map(std::str::from_utf8)
+            .collect::<Result<Vec<&str>, _>>()
+            .unwrap()
+            .join("_")
+    );
 
     let _instant_before_octree = Instant::now();
     let mut octree = unsafe {
