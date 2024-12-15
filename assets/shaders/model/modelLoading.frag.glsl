501d832618dd83d881a4319864b4f7e9ea137228
Francisco Aguirre <franciscoaguirreperez@gmail.com>
2024-12-15 20:23:42 -0300

    fix: get diffuse from actual obj material

--- assets/shaders/model/modelLoading.frag.glsl
+++ assets/shaders/model/modelLoading.frag.glsl
@@ -8,6 +8,9 @@ in vec3 fragNormal;
 uniform bool hasTexture;
 uniform sampler2D texture_diffuse1;
 
+uniform bool hasDiffuse;
+uniform vec3 materialDiffuse;
+
 struct Material {
     vec3 color;
     float diffuse;
@@ -19,6 +22,8 @@ void main()
 {
     if (hasTexture) {
         FragColor = texture(texture_diffuse1, fragTexCoords);
+    } else if (hasDiffuse) {
+        FragColor = vec4(materialDiffuse, 1);
     } else {
         FragColor = vec4(material.color, 1);
     }
