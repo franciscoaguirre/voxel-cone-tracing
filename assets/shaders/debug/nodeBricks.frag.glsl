545b28cc1f39081f8caf53296d1b911ec8c71767
Felipe Pizzorno <felipepizzorno98@gmail.com>
2024-12-15 15:33:50 -0300

    Minor fixes

--- assets/shaders/debug/nodeBricks.frag.glsl
+++ assets/shaders/debug/nodeBricks.frag.glsl
@@ -5,5 +5,8 @@ layout (location = 0) out vec4 FragColor;
 in flat vec4 frag_nodeColor;
 
 void main() {
+    if (frag_nodeColor.x < 0.1 && frag_nodeColor.y < 0.1 && frag_nodeColor.z < 0.1) {
+        discard;
+    }
     FragColor = frag_nodeColor;
 }
--- assets/shaders/octree/visualizeBricks.frag.glsl
+++ assets/shaders/octree/visualizeBricks.frag.glsl
@@ -5,7 +5,7 @@ layout (location = 0) out vec4 FragColor;
 in flat vec4 frag_nodeColor;
 
 void main() {
-    if (frag_nodeColor == vec4(0)) {
+    if (frag_nodeColor.x < 0.2 || frag_nodeColor.y < 0.2 || frag_nodeColor.z < 0.2) {
         discard;
     }
     FragColor = frag_nodeColor;
