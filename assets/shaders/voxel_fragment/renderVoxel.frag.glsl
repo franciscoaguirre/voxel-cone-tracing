e132843a99ddb7b17b6540d9d075ea1980b7c314
Felipe Pizzorno <felipepizzorno98@gmail.com>
2024-12-15 17:10:27 -0300

    Remove diffuse for voxel visualization

--- assets/shaders/voxel_fragment/renderVoxel.frag.glsl
+++ assets/shaders/voxel_fragment/renderVoxel.frag.glsl
@@ -8,5 +8,5 @@ in flat vec4 frag_nodeColor;
 void main() {
     vec3 pointOfView = vec3(0.25,0.5,-1.0);
     float diffuse = abs(dot(normalize(frag_normal), pointOfView)); 
-    FragColor = vec4(frag_nodeColor.xyz * diffuse, 1.0);
+    FragColor = vec4(frag_nodeColor.xyz, 1.0);
 }
